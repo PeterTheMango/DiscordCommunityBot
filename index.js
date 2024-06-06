aba53af13b7e805b98194cabfc682e5c71511257
PeterTheMango <72300659+PeterTheMango@users.noreply.github.com>
2024-06-06 09:35:10 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// npm i discord.js ms moment moment-duration-format glob util path colors
-
-const {
-    init
-} = require(`./Structures/Client`);
-const Config = require(`./Assets/Config.json`);
-const fs = require(`fs`);
-
-init(Config);
